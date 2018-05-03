9eef60c99f4e234f682bacb74ef74b2cf05d2bc1
Raefe <raefe.nj@gmail.com>
2018-05-03 13:40:25 +0100

    changing indentation

--- iris/iris.py
+++ iris/iris.py
@@ -17,11 +17,11 @@ class Iris():
         return train_dataset_fp
 
     def parse_csv(line):
-      example_defaults = [[0.], [0.], [0.], [0.], [0]]
-      parsed_line = tf.decode_csv(line, example_defaults)
-      features = tf.reshape(parsed_line[:-1], shape=(4,))
-      label = tf.reshape(parsed_line[-1], shape=())
-      return features, label
+        example_defaults = [[0.], [0.], [0.], [0.], [0]]
+        parsed_line = tf.decode_csv(line, example_defaults)
+        features = tf.reshape(parsed_line[:-1], shape=(4,))
+        label = tf.reshape(parsed_line[-1], shape=())
+        return features, label
 
     def format_data():
         train_dataset = tf.data.TextLineDataset(Iris.download_data())
