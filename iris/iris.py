1242c587baf47b6d83feee54d5f8e2b931b10c33
George Drayson <drayson.g@mac.com>
2018-05-03 17:17:42 +0100

    Added train method with tests

--- iris/iris.py
+++ iris/iris.py
@@ -16,6 +16,10 @@ class Iris:
             tf.keras.layers.Dense(10, activation="relu"),
             tf.keras.layers.Dense(3)
         ])
+        self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+        self.train_loss_results = []
+        self.train_accuracy_results = []
+        self.epoch_number = 201
 
     def download_data(self):
         train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
@@ -23,18 +27,48 @@ class Iris:
                                            origin=train_dataset_url)
         return train_dataset_fp
 
-    def parse_csv(self, line):
+    def format_data(self):
+        train_dataset = tf.data.TextLineDataset(self.download_data())
+        train_dataset = train_dataset.skip(1)
+        train_dataset = train_dataset.map(self.__parse_csv)
+        train_dataset = train_dataset.shuffle(buffer_size=1000)
+        train_dataset = train_dataset.batch(32)
+        features, label = tfe.Iterator(train_dataset).next()
+        return features, label, train_dataset
+
+    def train(self):
+        for epoch in range(self.epoch_number):
+            epoch_loss_avg = tfe.metrics.Mean()
+            epoch_accuracy = tfe.metrics.Accuracy()
+
+            for x, y in tfe.Iterator(self.format_data()[2]):
+                grads = self.__grad(self.model, x, y)
+                self.optimizer.apply_gradients(zip(grads, self.model.variables),
+                                               global_step=tf.train.get_or_create_global_step())
+                epoch_loss_avg(self.__loss(self.model, x, y))
+                epoch_accuracy(tf.argmax(self.model(x), axis=1, output_type=tf.int32), y)
+            self.train_loss_results.append(epoch_loss_avg.result())
+            self.train_accuracy_results.append(epoch_accuracy.result())
+
+            if epoch % 20 == 0:
+                print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+                                                                epoch_loss_avg.result(),
+                                                                epoch_accuracy.result()))
+
+        return self.train_loss_results, self.train_accuracy_results
+
+    def __parse_csv(self, line):
         example_defaults = [[0.], [0.], [0.], [0.], [0]]
         parsed_line = tf.decode_csv(line, example_defaults)
         features = tf.reshape(parsed_line[:-1], shape=(4,))
         label = tf.reshape(parsed_line[-1], shape=())
         return features, label
 
-    def format_data(self):
-        train_dataset = tf.data.TextLineDataset(self.download_data())
-        train_dataset = train_dataset.skip(1)
-        train_dataset = train_dataset.map(self.parse_csv)
-        train_dataset = train_dataset.shuffle(buffer_size=1000)
-        train_dataset = train_dataset.batch(32)
-        features, label = tfe.Iterator(train_dataset).next()
-        return features, label
+    def __loss(self, model, x, y):
+        y_ = model(x)
+        return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+
+    def __grad(self, model, inputs, targets):
+        with tfe.GradientTape() as tape:
+            loss_value = self.__loss(model, inputs, targets)
+        return tape.gradient(loss_value, model.variables)
--- iris/test_iris.py
+++ iris/test_iris.py
@@ -13,12 +13,22 @@ class IrisTest(tf.test.TestCase):
         self.assertAllEqual(filepath ,'iris_training.csv')
 
     def test_format_data_features_is_tensor(self):
-        features, label = self.iris.format_data()
+        features, label, dataset = self.iris.format_data()
         self.assertTrue(isinstance(features[0], tf.Tensor))
 
     def test_format_data_label_is_tensor(self):
-        features, label = self.iris.format_data()
+        features, label, dataset = self.iris.format_data()
         self.assertTrue(isinstance(label[0], tf.Tensor))
+
+    def test_train_function_adds_to_loss_array(self):
+        train_loss_results = self.iris.train()[0]
+        self.assertAllEqual(len(train_loss_results), 201)
+
+    def test_train_function_adds_to_accuracy_array(self):
+        train_accuracy_results = self.iris.train()[1]
+        self.assertAllEqual(len(train_accuracy_results), 201)
+
+
 
 if __name__ == '__main__':
     tf.test.main()
