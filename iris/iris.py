6535f4a6084e39510d2467dfb6d36b1cdacd8df4
Raefe <raefe.nj@gmail.com>
2018-05-03 14:16:08 +0100

    added model setup in intialize method and changed test setup accordingly

--- iris/iris.py
+++ iris/iris.py
@@ -8,25 +8,32 @@ import tensorflow.contrib.eager as tfe
 
 tf.enable_eager_execution()
 
-class Iris():
+class Iris:
 
-    def download_data():
+    def __init__(self):
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),
+            tf.keras.layers.Dense(10, activation="relu"),
+            tf.keras.layers.Dense(3)
+        ])
+
+    def download_data(self):
         train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
         train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
                                            origin=train_dataset_url)
         return train_dataset_fp
 
-    def parse_csv(line):
+    def parse_csv(self, line):
         example_defaults = [[0.], [0.], [0.], [0.], [0]]
         parsed_line = tf.decode_csv(line, example_defaults)
         features = tf.reshape(parsed_line[:-1], shape=(4,))
         label = tf.reshape(parsed_line[-1], shape=())
         return features, label
 
-    def format_data():
-        train_dataset = tf.data.TextLineDataset(Iris.download_data())
+    def format_data(self):
+        train_dataset = tf.data.TextLineDataset(self.download_data())
         train_dataset = train_dataset.skip(1)
-        train_dataset = train_dataset.map(Iris.parse_csv)
+        train_dataset = train_dataset.map(self.parse_csv)
         train_dataset = train_dataset.shuffle(buffer_size=1000)
         train_dataset = train_dataset.batch(32)
         features, label = tfe.Iterator(train_dataset).next()
--- iris/test_iris.py
+++ iris/test_iris.py
@@ -5,16 +5,19 @@ import tensorflow as tf
 
 class IrisTest(tf.test.TestCase):
 
+    def setUp(self):
+        self.iris = Iris()
+
     def test_download_function(self):
-        filepath = Iris.download_data()[-17:]
+        filepath = self.iris.download_data()[-17:]
         self.assertAllEqual(filepath ,'iris_training.csv')
 
     def test_format_data_features_is_tensor(self):
-        features, label = Iris.format_data()
+        features, label = self.iris.format_data()
         self.assertTrue(isinstance(features[0], tf.Tensor))
 
     def test_format_data_label_is_tensor(self):
-        features, label = Iris.format_data()
+        features, label = self.iris.format_data()
         self.assertTrue(isinstance(label[0], tf.Tensor))
 
 if __name__ == '__main__':
