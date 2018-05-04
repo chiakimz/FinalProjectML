647efef493b66af3872647ee3cdcb4285b5adf82
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-05-04 13:07:33 +0100

    model can now be restored

--- iris/controller.py
+++ iris/controller.py
@@ -12,3 +12,10 @@ from iris import Iris
 
 iris = Iris()
 iris.train()
+
+w1 = tf.contrib.eager.Variable(tf.random_normal(shape=[2]), name='w1')
+w2 = tf.contrib.eager.Variable(tf.random_normal(shape=[5]), name='w2')
+saver = tf.train.Saver([w1,w2])
+sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+saver.save(sess, './iris/my_test_model')
