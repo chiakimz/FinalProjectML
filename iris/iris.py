0b11304cbab8926a9ce1092f013f08cde9b5dccd
Raefe <raefe.nj@gmail.com>
2018-05-05 18:28:32 +0100

    deleting picture files

--- iris/iris.py
+++ iris/iris.py
@@ -26,7 +26,6 @@ class Iris:
 
     def download_test_data(self):
         test_url = "http://download.tensorflow.org/data/iris_test.csv"
-
         test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
                                   origin=test_url)
         return test_fp
