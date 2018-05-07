e2c91cca844ee23b333a09ada673f71651014f6a
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-05-07 12:59:31 +0100

    initial setup done, ready for TDD to begin

--- ml-app/src/App.js
+++ ml-app/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import ProjectList from './components/ProjectList';
 class App extends Component {
-  render() {
+constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {id: 1, name: 'Iris', brand: 'Supervised Learning'},
+        {id: 2, name: 'Something', brand: 'Supervised Learning'},
+        {id: 3, name: 'Something', brand: 'Supervised Learning'},
+      ]
+    }
+  }
+render() {
     return (
-      <div className="App">
-        Wolves of Commercial St.
+      <div>
+        <h1>Wolves of Commercial Street Projects</h1>
+        <ProjectList projects={this.state.projects}/>
       </div>
     );
   }
 }
-
 export default App;
---
+++ ml-app/src/components/ProjectList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+function ProjectList (props) {
+  return (
+    <div>ProjectList</div>
+  )
+}
+ProjectList.propTypes = {
+  projects: PropTypes.array.isRequired
+};
+export default ProjectList;
