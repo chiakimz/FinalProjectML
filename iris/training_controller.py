3373fd7f229691c9f2625d519dad9210a6acbc18
George Drayson <drayson.g@mac.com>
2018-05-07 23:02:30 +0100

    Got to 0.012 loss and 99.827% accuracy with creditcard data

--- iris/training_controller.py
+++ iris/training_controller.py
@@ -9,13 +9,14 @@ import tensorflow.contrib.eager as tfe
 tf.enable_eager_execution()
 
 from iris import Iris
+from fraud import Fraud
 
-iris = Iris()
-trained_iris = iris.train()
-iris.graph(trained_iris, 'graphs')
-model = iris.model
+fraud = Fraud()
+trained_fraud = fraud.train()
+fraud.graph(trained_fraud, 'fraud_graphs')
+model = fraud.model
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-checkpoint_dir = './iris_model'
+checkpoint_dir = './fraud_model'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 root = tfe.Checkpoint(optimizer=optimizer,
                       model=model,
