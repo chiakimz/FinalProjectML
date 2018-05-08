8e7456416221e4e77eac476a4478141277548797
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-05-08 11:09:19 +0100

    fixed errors that were occuring when test is run

--- ml-app/src/components/ProjectList.test.js
+++ ml-app/src/components/ProjectList.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import ProjectList from './ProjectList';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 it('should render a list of products as an unordered list', () => {
-  const mockProducts = [
+  const mockProjects = [
     {id: 1, name: 'Iris', brand: 'Supervised Learning'},
     {id: 2, name: 'Fraud', brand: 'Supervised Learning'},
     {id: 3, name: 'Diabetes', brand: 'Supervised Learning'},
