dc86467ac07a3789b0649db269eb2b082c9a8ab4
George Drayson <drayson.g@mac.com>
2018-05-08 14:39:35 +0100

    Adding iris folder that has been sorted and given separate controllers

--- iris/iris_flower_categoriser/iris_training_controller.py
+++ iris/iris_flower_categoriser/iris_training_controller.py
@@ -8,18 +8,14 @@ import tensorflow.contrib.eager as tfe
 
 tf.enable_eager_execution()
 
-import sys
-sys.path.insert(0, './iris_flower_categoriser')
 from iris import Iris
-sys.path.insert(0, './creditcard_fraud_detector')
-from fraud import Fraud
 
 iris = Iris()
 trained_iris = iris.train()
-iris.graph(trained_iris, './iris_flower_categoriser/iris_graphs')
+iris.graph(trained_iris, 'iris_graphs')
 model = iris.model
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-checkpoint_dir = './iris_flower_categoriser/iris_model'
+checkpoint_dir = './iris_model'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 root = tfe.Checkpoint(optimizer=optimizer,
                       model=model,
