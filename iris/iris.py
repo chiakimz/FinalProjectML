bcaca288416720dd2e8fd80e153f47071f81ed1f
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-05-09 10:56:17 +0100

    refactored graph function

--- iris/iris.py
+++ iris/iris.py
@@ -16,6 +16,7 @@ class Iris:
         self.train_loss_results = []
         self.train_accuracy_results = []
         self.epoch_number = 201
+        self.class_ids = ["Iris setosa", "Iris versicolor", "Iris virginica"]
 
     def download_data(self):
         train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
@@ -23,8 +24,14 @@ class Iris:
                                            origin=train_dataset_url)
         return train_dataset_fp
 
-    def format_data(self):
-        train_dataset = tf.data.TextLineDataset(self.download_data())
+    def download_test_data(self):
+        test_url = "http://download.tensorflow.org/data/iris_test.csv"
+        test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                  origin=test_url)
+        return test_fp
+
+    def format_data(self, data_filepath):
+        train_dataset = tf.data.TextLineDataset(data_filepath)
         train_dataset = train_dataset.skip(1)
         train_dataset = train_dataset.map(self.__parse_csv)
         train_dataset = train_dataset.shuffle(buffer_size=1000)
@@ -37,7 +44,7 @@ class Iris:
             epoch_loss_avg = tfe.metrics.Mean()
             epoch_accuracy = tfe.metrics.Accuracy()
 
-            for x, y in tfe.Iterator(self.format_data()[2]):
+            for x, y in tfe.Iterator(self.format_data(self.download_data())[2]):
                 grads = self.__grad(self.model, x, y)
                 self.optimizer.apply_gradients(zip(grads, self.model.variables),
                                                global_step=tf.train.get_or_create_global_step())
@@ -50,6 +57,29 @@ class Iris:
                 self.__print_report(epoch, epoch_loss_avg.result(), epoch_accuracy.result())
 
         return self.train_loss_results, self.train_accuracy_results
+
+    def test(self):
+        test_accuracy = tfe.metrics.Accuracy()
+
+        for (x, y) in tfe.Iterator(self.format_data(self.download_test_data())[2]):
+            prediction = tf.argmax(self.model(x), axis=1, output_type=tf.int32)
+            test_accuracy(prediction, y)
+
+        print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+        return "{:.3%}".format(test_accuracy.result())
+
+    def predict(self, data):
+        predict_dataset = tf.convert_to_tensor(data)
+
+        predictions = self.model(predict_dataset)
+        returned_predictions = []
+
+        for i, logits in enumerate(predictions):
+          class_idx = tf.argmax(logits).numpy()
+          name = self.class_ids[class_idx]
+          returned_predictions.append("Example {} prediction: {}".format(i+1, name))
+        print("\n".join(returned_predictions))
+        return "\n".join(returned_predictions)
 
     def __parse_csv(self, line):
         example_defaults = [[0.], [0.], [0.], [0.], [0]]
@@ -69,3 +99,21 @@ class Iris:
 
     def __print_report(self, epoch, epoch_loss, epoch_accuracy):
         print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch, epoch_loss, epoch_accuracy))
+
+    def graph(self, train_arg, image_folder_path):
+        fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+        fig.suptitle('Training Metrics')
+        self.__loss_plotter(train_arg[0], axes)
+        tf.summary.histogram("Loss results", train_arg[0])
+        self.__accuracy_plotter(train_arg[1], axes)
+        tf.summary.histogram("Accuracy results", train_arg[1])
+        plt.savefig(f'{image_folder_path}/figure.png')
+
+    def __loss_plotter(self, loss_results, axes):
+        axes[0].set_ylabel("Loss", fontsize=14)
+        axes[0].plot(loss_results)
+
+    def __accuracy_plotter(self, accuracy_results, axes):
+        axes[1].set_ylabel("Accuracy", fontsize=14)
+        axes[1].set_xlabel("Epoch", fontsize=14)
+        axes[1].plot(accuracy_results)
