bc788b0e68fe087b34d9dff8cf4a82686218a337
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-05-10 10:21:56 +0100

    added tensorboard funtion for cancer model

--- iris/cancer_predictor/cancer_training_controller.py
+++ iris/cancer_predictor/cancer_training_controller.py
@@ -10,14 +10,22 @@ tf.enable_eager_execution()
 
 from cancer import Cancer
 
-cancer = Cancer()
-trained_cancer = cancer.train()
-cancer.graph(trained_cancer, 'cancer_graphs')
-model = cancer.model
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-checkpoint_dir = './cancer_model'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-root = tfe.Checkpoint(optimizer=optimizer,
-                      model=model,
-                      optimizer_step=tf.train.get_or_create_global_step())
-root.save(file_prefix=checkpoint_prefix)
+global_step = tf.train.get_or_create_global_step()
+summary_writer = tf.contrib.summary.create_file_writer(
+    'cancer_model', flush_millis=10000)
+with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
+  cancer = Cancer()
+
+  trained_cancer = cancer.train()
+  cancer.graph(trained_cancer, 'cancer_graphs')
+  model = cancer.model
+  optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+  checkpoint_dir = './cancer_model'
+  checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+  root = tfe.Checkpoint(optimizer=optimizer,
+                        model=model,
+                        optimizer_step=tf.train.get_or_create_global_step())
+  root.save(file_prefix=checkpoint_prefix)
+
+
+  tf.contrib.summary.scalar("loss", my_loss)
